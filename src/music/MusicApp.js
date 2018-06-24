5c90313acf741e82d724456aee21871eec61cff1
patrickdai <patrick.dai@contentsquare.com>
2018-06-24 16:57:38 +0200

    Add /music route

---
+++ src/music/MusicApp.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class MusicApp extends Component {
+  render() {
+    return (
+      <div className="Music-App"> This is music app</div>
+    );
+  }
+}
