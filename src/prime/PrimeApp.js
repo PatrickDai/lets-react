947cb58247571205a612c10f197ea237ae52e077
patrickdai <patrick.dai@contentsquare.com>
2018-06-24 15:41:21 +0200

    Add /prime route

---
+++ src/prime/PrimeApp.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class PrimeApp extends Component {
+  render() {
+    return (
+      <div className="Prime-App"> This is prime number app</div>
+    );
+  }
+}
