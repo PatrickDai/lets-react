67dfdb3dd4850179c4bf52df2ac5082cc41b4196
patrickdai <patrick.dai@contentsquare.com>
2018-06-24 16:20:32 +0200

    Prime app baby step

--- src/prime/PrimeApp.js
+++ src/prime/PrimeApp.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react';
+import getNthPrime from './prime.service';
 
 export default class PrimeApp extends Component {
+  state = { results: [] };
+
   render() {
     return (
-      <div className="Prime-App"> This is prime number app</div>
+      <div className="Prime-App">
+        <h1> Find the nth prime number </h1>
+        <PrimeForm onSubmit={this.handleSubmit.bind(this)}/>
+        <PrimeResults results={this.state.results}/>
+      </div>
     );
   }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const input = event.target.index;
+    const index = input.value;
+    const prime = getNthPrime(index);
+    this.updateState(index, prime);
+    this.emptyValue(input);
+  }
+
+  updateState(index, prime) {
+    const results = this.state.results.slice();
+    results.unshift({ index, prime });
+    this.setState({ results });
+  }
+
+  emptyValue(input) {
+    input.value = "";
+  }
+}
+
+function PrimeForm(props) {
+  const { onSubmit } = props;
+  return (
+    <form onSubmit={onSubmit}>
+      <input name="index" type="number" min="1" placeholder="Enter a number" required/>
+      <input type="submit"/>
+    </form>
+  );
+}
+
+function PrimeResults(props) {
+  const results = props.results.map((result, i) =>
+    <li key={i}> The prime number n°{result.index} is {result.prime} </li>
+  );
+  return <ul>{results}</ul>;
 }
