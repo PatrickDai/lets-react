5d1bddd230ec5a18f5e2e7820dec62abd0f45751
patrickdai <patrick.dai@contentsquare.com>
2018-06-24 17:35:06 +0200

    Music app baby step (mocked data)

--- src/music/MusicApp.js
+++ src/music/MusicApp.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import musics from './music.service'
 
 export default class MusicApp extends Component {
   render() {
     return (
-      <div className="Music-App"> This is music app</div>
+      <div className="Music-App">
+        <h1> Music App </h1>
+        <MusicList musics={musics}/>
+      </div>
     );
   }
+}
+
+function MusicList(props) {
+  const header = <tr>
+    <th>Title</th>
+    <th>Artist</th>
+    <th>Duration</th>
+  </tr>;
+  const rows = props.musics.map((music, i) =>
+    <tr key={i}>
+      <td>{music.title}</td>
+      <td>{music.artist}</td>
+      <td>{music.duration}</td>
+    </tr>
+  );
+  return (
+    <table>
+      <thead>{header}</thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
 }
